51193d56c99831b79a283571a75ff58737ce97f1
肥羊菌_ <50106548+feiyangjun-1@users.noreply.github.com>
2026-07-20 19:21:54 +0800

    Delete open_qa.bat

--- open_qa.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0"
-if exist "work_hanzistyleforge_fusion_months\qa\index.html" start "" "work_hanzistyleforge_fusion_months\qa\index.html"
-if not exist "work_hanzistyleforge_fusion_months\qa\index.html" echo QA report is not available yet.
-pause
